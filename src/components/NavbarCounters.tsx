0d24f18fd179779a0f2ecb993643f2f955cf73d2
Rahat Karim <rahatkarim5452@gmail.com>
2026-09-26 17:18:30 +0600

    NavbarCounter file edited

--- src/components/NavbarCounters.tsx
+++ src/components/NavbarCounters.tsx
@@ -8,18 +8,22 @@ const NavbarCounters = () => {
   const { plan, saved } = useContext(FitLogContext);
 
   return (
-    <div className="flex items-center gap-4 text-sm">
-      <Link href="/my-plan" className="flex items-center gap-2">
+    <div className="flex items-center gap-2 text-sm">
+      <Link
+        href="/my-plan"
+        className="flex items-center gap-1 transition hover:bg-white/10  rounded-full py-1 px-1"
+      >
         <span>Plan</span>
-
         <span className="rounded-full bg-lime-400  px-2 py-1 text-black">
           {plan.length}
         </span>
       </Link>
 
-      <Link href="/my-plan" className="flex items-center gap-2">
+      <Link
+        href="/my-plan"
+        className="flex items-center gap-1 transition hover:bg-white/10 rounded-full py-1 px-1"
+      >
         <span>Saved</span>
-
         <span className="rounded-full border border-white/30 px-2 py-1">
           {saved.length}
         </span>
